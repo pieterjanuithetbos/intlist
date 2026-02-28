820eaefcfcbcbe6dec01ce1da5e41678ba545283
pjuithetbos <pieter-jan.bossuyt@student.kuleuven.be>
2026-02-28 14:33:19 +0100

    correctie implementatie nodes

--- intlinst/src/intlinst/IntList.java
+++ intlinst/src/intlinst/IntList.java
@@ -14,19 +14,75 @@ import java.util.stream.IntStream;
  */
 public class IntList {
 	
+	private class Node {
+		int value;
+		Node next;
 		
+		Node(int element, Node next) {
+			this.value = element;
+			this.next = next;
+		}
+	}
+	
+	private Node[] getNodes() {
+		Node[] result = new Node[length];
+		Node k = head;
+		for (int i = 0; i < length; i++) {
+			if (k==null) {
+				return null;
+			}
+			result[i] = k;
+			k = k.next;
+		}
+		// return null als het onbject langer is dan 'length'
+		if (k!= null) {
+			return null;
+		}
+		return result;
+	}
+	
+	/**
+	 * @invar | 0 <= length
+	 * @invar | getNodes() != null
+	 * TODO die laatste begrijp ik niet helemaal
+	 */
+	private Node head;
+	private int length;
+	
 	public int getLength() {
-		;
+		Node next = this.next;
+		int counter = 1;
+		while (next != null) {
+			next = this.next;
+			counter++;
+		};
 	}
 	/**
 	 * @pre | 0 <= index && index <= getLength()
 	 */
 	public int getElement(int index) {
-		;
+		Node k = head;
+		for (int i=0; i<index;i++) {
+			k = k.next;
+		};
+		return k.value;
 	}
+	
+	/**
+	 * TODO !!
+	 * @creates | result
+	 * @post | result != null
+	 */
 	public int[] getArray() {
-		;
+		int[] result = new int[length];
+		Node k = head;
+		for (int i=0; i<length;i++) {
+			result[i] = k.value;
+			k = k.next;
+		};
+		return result.clone();
 	}
+	
 	
 	/**
 	 * @creates | this
@@ -34,7 +90,7 @@ public class IntList {
 	 * @post | getArray() == new int[] {}
 	 */
 	public IntList() {
-		;
+		this.head = null;
 	}
 	
 	/**
@@ -45,14 +101,26 @@ public class IntList {
 	 * @post Het laatste element van de nieuwe lijst is b
 	 */
 	public void add(int b) {
-		;
+		Node k = this.head;
+		int counter = 1;
+		while (k != null) {
+			k = k.next;
+			counter++;
+		};
+		k.next = new Node(0, null);
 	}
 	
 	/**
 	 * cf add
 	 */
 	public void remove(int c) {
-		;
+		Node next = this.next;
+		int counter = 1;
+		while (next != null) {
+			next = this.next;
+			counter++;
+		};
+		next = new Node(0, null);
 	}
 	
 }
